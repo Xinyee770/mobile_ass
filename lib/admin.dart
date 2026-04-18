f252ce3c15ec8453cc0cc08f55b816c949211e04
Xinyee <xin1123yee@gmail.com>
2026-04-18 21:51:53 +0800

    Add button to let us can connect to our page

---
+++ lib/admin.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Admin extends StatelessWidget {
+  const Admin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Admin Panel")),
+      body: const Center(child: Text("Admin Screen")),
+    );
+  }
+}
